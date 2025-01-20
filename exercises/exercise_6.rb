e9d118bfe50a0cddb9fb116da3cc477eada3f795
DemouraGus <demoura.gus@gmail.com>
2025-01-20 12:57:08 -0800

    ex7 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,10 +11,10 @@ puts "----------"
 # Your code goes here ...
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 25)
-@store1.employees.create(first_name: "John", last_name: "Lennon", hourly_rate: 17)
-@store1.employees.create(first_name: "Kurt", last_name: "Cobain", hourly_rate: 260)
-@store2.employees.create(first_name: "Sid", last_name: "Vicious", hourly_rate: 3)
+@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 41)
+@store1.employees.create(first_name: "John", last_name: "Lennon", hourly_rate: 42)
+@store1.employees.create(first_name: "Kurt", last_name: "Cobain", hourly_rate: 180)
+@store2.employees.create(first_name: "Sid", last_name: "Vicious", hourly_rate: 40)
 @store2.employees.create(first_name: "Paul", last_name: "McCartney", hourly_rate: 70)
-@store2.employees.create(first_name: "Mick", last_name: "Jagger", hourly_rate: 1060)
-@store2.employees.create(first_name: "Ash", last_name: "Ketchum", hourly_rate: 30)
+@store2.employees.create(first_name: "Mick", last_name: "Jagger", hourly_rate: 200)
+@store2.employees.create(first_name: "Ash", last_name: "Ketchum", hourly_rate: 42)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_some_apparel
+
+  private
+
+  def must_carry_some_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "Stores must carry at least one type of apparel.")
+    end
+  end
 end
