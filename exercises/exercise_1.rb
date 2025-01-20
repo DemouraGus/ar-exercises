965978ac48337807b3288ebbe6d408452fc22834
DemouraGus <demoura.gus@gmail.com>
2025-01-20 12:12:50 -0800

    exs 1 and 2 complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,13 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.new(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.new(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.new(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+burnaby.save
+richmond.save
+gastown.save
+
+puts "Total number of stores: #{Store.count}."
